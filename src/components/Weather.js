b9a6ff285e42250e3b68876292a486ce20a7e7e9
Joe <musicdudejoe@googlemail.com>
2021-07-20 14:43:54 +0100

    Updated to allow searching for 5 days of weather in any city

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,19 +3,27 @@ import React, { useEffect, useState } from 'react';
 import '../styling/weather.scss';
 
 const Weather = () => {
-  const [weather, updateWeather] = useState(null)
+  const [weather, updateWeather] = useState(null);
+  const [searched_city="London", updateSearchedCity] = useState('');
 
   useEffect(() => {
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`).then(res => {
-      updateWeather(res.data.weather[0].description)
-    })
-  }, [])
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${searched_city}&cnt=5&appid=${process.env.REACT_APP_WEATHER_API_KEY}`).then(res => {
+      let forecast = []
+      for (let i = 0; i < res.data.list.length; i++) {
+        console.log(res.data.list[i].weather[0].description);
+        forecast.push(res.data.list[i].weather[0].description);
+      }
+      updateWeather(forecast);
+    }).catch(error => console.error(error))
+  }, [searched_city])
 
   return (
     <div data-testid="weather" className="weather">
+      <input type="text" id="city-input"></input>
+      <input type="button" value="Submit" onClick={() => updateSearchedCity(document.getElementById("city-input").value)}></input>
       Weather:
       <br />
-      The current weather in London is: {weather}
+      The weather in {searched_city} for the next 5 days is: {weather ? weather.join(', ') : ''}
     </div>
   );
 }
